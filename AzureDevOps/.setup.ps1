28882795fa66c6b495a46fde4d14fa8db08e4873
Justin <justinbeeson@gmail.com>
2020-06-11 23:22:31 -0700

    add az repos

---
+++ AzureDevOps/.setup.ps1
@@ -0,0 +1,23 @@
+# This file will contain sensitive information and should not be committed to the repo.
+$envVariables = Import-PowerShellDataFile -Path "$PSScriptRoot\.envVars.psd1"
+<# EXAMPLE
+@{
+    System_AccessToken = '*********************************************'
+    System_CollectionUri = 'https://dev.azure.com/[organization]'
+    System_TeamProject = 'PowerShellProject'
+    Build_RequestedFor = 'Justin Beeson'
+    Build_RequestedForEmail = 'jbeeson@organization.email'
+}
+#>
+
+foreach ($var in $envVariables.Keys) {
+    [System.Environment]::SetEnvironmentVariable($var, $envVariables[$var], 'User')
+    New-Item -Path "env:$var" -Value $envVariables[$var] -Force
+}
+
+if (!( Get-InstalledModule -Name PSSiOps -ErrorAction SilentlyContinue )) {
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+    iex ((New-Object System.Net.WebClient).DownloadString('https://sidevbin.blob.core.windows.net/files/PowerShell/Install-PSSiOps.ps1'))
+}
+
+Start-CmToolsSetup
---
+++ AzureDevOps/.sync-azrepos.ps1
@@ -0,0 +1,26 @@
+param (
+    [String]$UserName = $env:Build_RequestedFor,
+    [String]$UserEmail = $env:Build_RequestedForEmail
+)
+
+$repos = Get-Content -Path "$PSScriptRoot\.azrepos"
+$reposPath = @( & "$PSScriptRoot\.path.ps1" )
+New-Item -Path $reposPath -ItemType Directory -Force
+$git = @{
+    FilePath = 'git'
+    NoNewWindow = $true
+    Wait = $true
+}
+Start-Process @git -ArgumentList 'config', '--global', 'user.name', $UserName
+Start-Process @git -ArgumentList 'config', '--global', 'user.email', $UserEmail
+foreach ($path in $reposPath) {
+    Push-Location -Path $path
+    foreach ($repo in $repos) {
+        $replacement = "https://anything:$env:System_AccessToken@"
+        Start-Process @git -ArgumentList (
+            'clone',
+            $repo -replace ('https:\/\/(([Nn]uva[Dd]ev@)|(?=dev.*))', $replacement)
+        )
+    }
+    Pop-Location
+}
