0b5f32cec06a6d96c2262c6f603c8ec2b815a14b
Justin <justinbeeson@gmail.com>
2020-06-15 16:15:45 -0700

    .dotfiles saved by dot update

--- AzureDevOps/.setup.ps1
+++ AzureDevOps/.setup.ps1
@@ -2,8 +2,13 @@ if (!( Get-Command Import-PowerShellDataFile -ErrorAction SilentlyContinue )) {
     Import-Module -Name "C:\WINDOWS\system32\WindowsPowerShell\v1.0\Modules\Microsoft.PowerShell.Utility\Microsoft.PowerShell.Utility.psd1"
 }
 # This file will contain sensitive information and should not be committed to the repo.
-$envVariables = Import-PowerShellDataFile -Path $PSScriptRoot\.envVars.psd1
-<# EXAMPLE
+try {
+    $envVariables = Import-PowerShellDataFile -Path $PSScriptRoot\.envVars.psd1
+}
+catch {
+    Write-Warning -Message "Couldn't import environment variables. Have you created $PSScriptRoot\.envVars.psd1?"
+    Write-Host -Object "Example:"
+    Write-Host -Object @"
 @{
     System_AccessToken = '*********************************************'
     System_CollectionUri = 'https://dev.azure.com/[organization]'
@@ -11,7 +16,8 @@ $envVariables = Import-PowerShellDataFile -Path $PSScriptRoot\.envVars.psd1
     Build_RequestedFor = 'Justin Beeson'
     Build_RequestedForEmail = 'jbeeson@organization.email'
 }
-#>
+"@
+}
 
 foreach ($var in $envVariables.Keys) {
     [System.Environment]::SetEnvironmentVariable($var, $envVariables[$var], 'User')
