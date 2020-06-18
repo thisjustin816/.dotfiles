7a54d2d074eea38004998b7c4d6f77826cd9113d
Justin <justinbeeson@gmail.com>
2020-06-17 21:43:44 -0700

    fix error when no profile is found

--- PowerShell/.setup.ps1
+++ PowerShell/.setup.ps1
@@ -1,3 +1,4 @@
-$profile.CurrentUserAllHosts | Remove-Item -Force -ErrorAction SilentlyContinue
-( pwsh -Command '$profile.CurrentUserAllHosts' ) | Remove-Item -Force -ErrorAction SilentlyContinue
+"$($profile.CurrentUserAllHosts)" | Remove-Item -Force -ErrorAction SilentlyContinue
+( pwsh -Command '"$($profile.CurrentUserAllHosts)"' -ErrorAction SilentlyContinue ) |
+    Remove-Item -Force -ErrorAction SilentlyContinue
 . $PSScriptRoot\profile.ps1
