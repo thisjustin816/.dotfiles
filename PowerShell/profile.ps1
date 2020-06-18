3778cae75d557fee357a1548e40177f88824e984
Justin <justinbeeson@gmail.com>
2020-06-17 21:57:26 -0700

    don't import xml for credential

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -1,6 +1,6 @@
 $updateCheckPath = "$( $profile.CurrentUserAllHosts | Split-Path -Parent )\LastPSSiOpsUpdateCheck.xml"; if (!( Get-Item -Path $updateCheckPath -ErrorAction SilentlyContinue ) -or (( Import-Clixml -Path $updateCheckPath ) -lt ( Get-Date ).AddDays(-1))) { Import-Module -Name PSSiSharedFunctions -MinimumVersion 2.0.13 -Force; Get-PSSiOpsUpdate -ErrorAction SilentlyContinue }
 
-<# Begin AzCredential #> $AzCredential = Import-Clixml -Path "C:\Users\jbeeson\OneDrive - Nuvasive Inc\Documents\WindowsPowerShell\AzNugetCredential.xml"; $PSDefaultParameterValues = @{ "*-Module:Credential" = $AzCredential; "*-Package:Credential" = $AzCredential; "*-PackageSource:Credential" = $AzCredential; "*-PackageProvider:Credential" = $AzCredential; "*-PSRepository:Credential" = $AzCredential } <# End AzCredential #>
+<# Begin AzCredential #> $AzCredential = Get-PatPSCredential; $PSDefaultParameterValues = @{ "*-Module:Credential" = $AzCredential; "*-Package:Credential" = $AzCredential; "*-PackageSource:Credential" = $AzCredential; "*-PackageProvider:Credential" = $AzCredential; "*-PSRepository:Credential" = $AzCredential } <# End AzCredential #>
 
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
