54c3fa785efafb89512e53259bbafb58a3023529
Justin <justinbeeson@gmail.com>
2020-07-13 07:37:51 -0700

    .dotfiles saved by dot save

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -11,5 +11,6 @@ if (Test-Path($ChocolateyProfile)) {
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme Paradox
+
 <# Begin AzCredential #> $AzCredential = Import-Clixml -Path "C:\Users\jbeeson\OneDrive - Nuvasive Inc\Documents\WindowsPowerShell\AzNugetCredential.xml"; $PSDefaultParameterValues = @{ "*-Module:Credential" = $AzCredential; "*-Package:Credential" = $AzCredential; "*-PackageSource:Credential" = $AzCredential; "*-PackageProvider:Credential" = $AzCredential; "*-PSRepository:Credential" = $AzCredential } <# End AzCredential #>
 $updateCheckPath = "$( Get-Item -Path $profile.CurrentUserAllHosts | Split-Path -Parent )\LastPSSiOpsUpdateCheck.xml"; if (!( Get-Item -Path $updateCheckPath -ErrorAction SilentlyContinue ) -or (( Import-Clixml -Path $updateCheckPath ) -lt ( Get-Date ).AddDays(-1))) { Import-Module -Name PSSiSharedFunctions -MinimumVersion 2.0.13 -Force; Get-PSSiOpsUpdate -ErrorAction SilentlyContinue }
