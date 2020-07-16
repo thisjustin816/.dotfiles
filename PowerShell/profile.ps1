be73758a169308b52a45a917ef2fe81b5357d1d4
Justin <justinbeeson@gmail.com>
2020-07-16 07:26:43 -0700

    don't throw an error if profile modules aren't installed yet

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -4,6 +4,6 @@ if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
 
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-Theme Paradox
+Import-Module posh-git -ErrorAction Ignore
+Import-Module oh-my-posh -ErrorAction Ignore
+if ( Get-Module posh-git, oh-my-posh -ErrorAction Ignore ) { Set-Theme Paradox }
