f686e73c61c6ee7b6bc9e79ccc3a50ddf8be9f7f
Justin <justinbeeson@gmail.com>
2020-07-16 06:37:04 -0700

    fix credential storage in profile to use with multiple computers

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -8,5 +8,5 @@ Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme Paradox
 
-<# Begin AzCredential #> $AzCredential = Import-Clixml -Path "C:\Users\jbeeson\OneDrive - Nuvasive Inc\Documents\WindowsPowerShell\AzNugetCredential.xml"; $PSDefaultParameterValues = @{ "*-Module:Credential" = $AzCredential; "*-Package:Credential" = $AzCredential; "*-PackageSource:Credential" = $AzCredential; "*-PackageProvider:Credential" = $AzCredential; "*-PSRepository:Credential" = $AzCredential } <# End AzCredential #>
+<# Begin AzCredential #> $AzCredential = Get-PatPSCredential; $PSDefaultParameterValues = @{ "*-Module:Credential" = $AzCredential; "*-Package:Credential" = $AzCredential; "*-PackageSource:Credential" = $AzCredential; "*-PackageProvider:Credential" = $AzCredential; "*-PSRepository:Credential" = $AzCredential } <# End AzCredential #>
 $updateCheckPath = "$( Get-Item -Path $profile.CurrentUserAllHosts | Split-Path -Parent )\LastPSSiOpsUpdateCheck.xml"; if (!( Get-Item -Path $updateCheckPath -ErrorAction SilentlyContinue ) -or (( Import-Clixml -Path $updateCheckPath ) -lt ( Get-Date ).AddDays(-1))) { Import-Module -Name PSSiSharedFunctions -MinimumVersion 2.0.13 -Force; Get-PSSiOpsUpdate -ErrorAction SilentlyContinue }
