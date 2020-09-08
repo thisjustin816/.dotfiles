cc066014fd4252f6167537d5b34e7b10e4c00a49
Justin <justinbeeson@gmail.com>
2020-09-08 07:00:21 -0700

    Don't reboot on windows update

--- Windows/.sync-updates.ps1
+++ Windows/.sync-updates.ps1
@@ -1,3 +1,3 @@
-Install-WindowsUpdate -MicrosoftUpdate -AcceptAll |
+Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot |
     Select-Object -Property KB, Title, Description, SupportUrl |
     Sort-Object -Property KB -Unique
