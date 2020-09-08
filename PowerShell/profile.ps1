49d5f608b93fed8542ef2dc0d624eb3f22933304
Justin <justinbeeson@gmail.com>
2020-09-08 10:31:11 -0700

    use current profile directory for update check

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -8,7 +8,7 @@ Import-Module posh-git -ErrorAction Ignore
 Import-Module oh-my-posh -ErrorAction Ignore
 if ( Get-Command Set-Theme -ErrorAction Ignore ) { Set-Theme Paradox }
 
-$updateCheckPath = "$( Get-Item -Path $profile.CurrentUserAllHosts | Split-Path -Parent )\LastPSSiOpsUpdateCheck.xml"; if (!( Get-Item -Path $updateCheckPath -ErrorAction SilentlyContinue ) -or (( Import-Clixml -Path $updateCheckPath ) -lt ( Get-Date ).AddDays(-1))) { Import-Module -Name PSSiSharedFunctions -MinimumVersion 2.0.13 -Force; Get-PSSiOpsUpdate -ErrorAction SilentlyContinue }
+$updateCheckPath = "$( Get-Item -Path $profile | Split-Path -Parent )\LastPSSiOpsUpdateCheck.xml"; if (!( Get-Item -Path $updateCheckPath -ErrorAction SilentlyContinue ) -or (( Import-Clixml -Path $updateCheckPath ) -lt ( Get-Date ).AddDays(-1))) { Import-Module -Name PSSiSharedFunctions -MinimumVersion 2.0.13 -Force; Get-PSSiOpsUpdate -ErrorAction SilentlyContinue }
 
 <# Begin AzCredential #> $PSDefaultParameterValues = @{ "*-Module:Credential" = Get-PatPSCredential; "*-Package:Credential" = Get-PatPSCredential; "*-PackageSource:Credential" = Get-PatPSCredential; "*-PackageProvider:Credential" = Get-PatPSCredential; "*-PSRepository:Credential" = Get-PatPSCredential } <# End AzCredential #>
 
