031c98eeef8784c35738a43f2540bad3c15b1a46
Justin <justinbeeson@gmail.com>
2022-02-01 09:24:37 -0800

    prompt for a reboot after updating windows

--- Windows/.sync-updates.ps1
+++ Windows/.sync-updates.ps1
@@ -1,3 +1,5 @@
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot |
     Select-Object -Property KB, Title, Description, SupportUrl |
     Sort-Object -Property KB -Unique
+
+Get-WURebootStatus
